57623e5087616063d77c5f561116802b7e85e66c
popkat412 <wangyunze412@gmail.com>
2023-04-25 16:37:05 +0800

    fix: type error

--- src/components/stopwatch.tsx
+++ src/components/stopwatch.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import SetScoreFn from "../pages/puzzle/types/setScoreFn";
 import formatTime from "../utils/formattime";
 
 interface StopWatchProps {
-    setTimeElapsed: SetScoreFn;
+    setTimeElapsed: React.Dispatch<React.SetStateAction<number>>;
     timeElapsed: number;
 }
 
