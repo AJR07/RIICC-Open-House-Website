da02d44e6f22320b212c97af6630c7eb8e04c004
AJR <angjunray@live.com>
2023-04-27 21:56:09 +0800

    :lipstick: Format home page!

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -15,18 +15,38 @@ export default function Home() {
                 paddingRight: "10%",
             }}
         >
+            <img
+                src="/logo.png"
+                alt="logo"
+                width="200px"
+                height="200px"
+                style={{ border: "solid white" }}
+            />
             <h1>Instructions for Open House Puzzles </h1>
-            <body>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Tempora modi enim consectetur quam ex quisquam aspernatur
-                consequatur atque veniam, suscipit deleniti iusto nam nesciunt
-                maxime excepturi molestias qui officiis minus quo rerum non
-                blanditiis? Tempora dolor nisi quasi perferendis non, laudantium
-                nam vel impedit eum cupiditate! Quam culpa magni numquam.
+            <body style={{ textAlign: "center" }}>
+                Welcome to RIICC's Open House Puzzle/Game Website! We have
+                designed 3 games for you to try out.
+                <br />
+                Each game is specifically designed to test your problem solving
+                skills, logical thinking or other skill-sets related to
+                programming.
+                <br />
+                The website will ask for your name after each game, after which
+                your obtained score could be recorded under your name, so you
+                can see how you compare to others!
             </body>
-            <Button variant="contained" onClick={() => navigate("/puzzle")}>
-                Go to Puzzle
-            </Button>
+            <h2>Most importantly, HAVE FUN!</h2>
+            <Stack spacing={5}>
+                <Button variant="contained" onClick={() => navigate("/puzzle")}>
+                    Go to Puzzle
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={() => navigate("/leaderboard")}
+                >
+                    Go to Leaderboards
+                </Button>
+            </Stack>
         </Stack>
     );
 }
